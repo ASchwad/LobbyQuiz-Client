b144e493830a6f72eb56c295116e36e0a4e2d399
Alex Schoenenwald <a.schoenenwald@gmx.de>
2019-01-22 19:06:06 +0100

    init commit

---
+++ src/components/Welcome.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export class Welcome extends React.Component{
+
+    onClickHandleStart(){
+        props.handleStart();
+    }
+
+    render() {
+        return (
+            <div id="main">
+                <div id="welcomeElement">
+                    <h3>With Lobby Quiz you can compare your League performance with your Lobby mates and find out the strengths and weaknesses of your Lobby.</h3>
+                    <h1>Are you ready to play?</h1>
+                    <button type="button" className="btn btn-lg black-background white" onClick={this.onClickHandleStart}>Hell Yeah!</button>
+                    <br />
+                    <hr class="divider"></hr>
+                    <div id="recommendation">
+                        <b>Before you start:</b>
+                        <p>This Quiz uses your overall ranked solo/duo queue games. Please make sure that...</p>
+                        <li>you have more than 10 games in this queue.</li>
+                        <li>you have your League Client opened.</li>
+                        <li>you created a Lobby and your friends joined you.</li>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,143 +2,64 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Square(props) {
-      return (
-        <button className="square" onClick={props.onClick}>
-          {props.value}
-        </button>
-      );
-  }
-  
-class Board extends React.Component {
-    renderSquare(i) {
-      return <Square 
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
-      />;
-    }
-  
-    render() {
-      return (
-        <div>
-          <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-          </div>
-          <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  class Game extends React.Component {
+//import {Welcome} from "./components/Welcome";
+
+class Game extends React.Component {
 
     constructor (props) {
         super(props);
         this.state = {
-            history: [{
-                squares: Array(9).fill(null)
-            }],
-            xIsNext: true,
-            stepNumber: 0
+            players: [],
+            quiz: {},
+            welcomeDone: false
         }
+
+        this.handleStart = this.handleStart.bind(this);
     };
 
-    handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1)
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-          return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-          history: history.concat([{
-            squares: squares,
-          }]),
-          xIsNext: !this.state.xIsNext,
-          stepNumber: history.length
+    handleStart() {
+        var request = require('request');
+        request('http://localhost:8081/playQuiz', (error, response, body) => {
+            var parsedBody = JSON.parse(body);
+            this.setState({
+                players: parsedBody.quiz.participants,
+                questions: parsedBody.quiz.questions,
+                welcomeDone: true
+            });
         });
-    }
-    jumpTo(step){
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step%2) === 0
-        })
-    }
+    };
+
+
     render() {
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
-        
- 
-        const moves = history.map((step, move) => {
-            const desc = move ?
-              'Go to move #' + move :
-              'Go to game start';
-            return (
-              <li key={move}>
-                <button onClick={() => this.jumpTo(move)}>{desc}</button>
-              </li>
-            );
-        });
-
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-
+        //const welcomeDone = this.state.welcomeDone;
         return (
-            <div className="game">
-            <div className="game-board">
-                <Board squares={current.squares}
-                onClick={(i) => this.handleClick(i)}/>
+            <div id="main">
+                <div id="welcomeElement">
+                    <h3>With Lobby Quiz you can compare your League performance with your Lobby mates and find out the strengths and weaknesses of your Lobby.</h3>
+                    <h1>Are you ready to play?</h1>
+                    <button type="button" className="btn btn-lg black-background white" onClick={this.handleStart}>Hell Yeah!</button>
+                    <br />
+                    <hr class="divider"></hr>
+                    <div id="recommendation">
+                        <b>Before you start:</b>
+                        <p>This Quiz uses your overall ranked solo/duo queue games. Please make sure that...</p>
+                        <li>you have more than 10 games in this queue.</li>
+                        <li>you have your League Client opened.</li>
+                        <li>you created a Lobby and your friends joined you.</li>
+                    </div>
+                </div>
             </div>
-            <div className="game-info">
-                <div>{status}</div>
-                <ol>{moves}</ol>
-            </div>
-            </div>
+            
         );
-    }
-  }
+    };
+
+    
+}
+
   
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-  // ========================================
-  
-  ReactDOM.render(
+ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
+);
   
   
